66e54156d6fe2b8a5c1abfdd6c826c731b445c54
ebeguin <ebeguin@gmail.com>
2020-08-13 16:55:06 +0200

    training 2

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .mocks import Post
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-	posts= Post.all()
+	posts= Post.objects.all()
 	return render(request,'blog/index.html',{'posts':posts})
 
 def show(request, id):
-	post = Post.find(id)
+	post = get_object_or_404(Post, pk=id)
 	return render (request,'blog/show.html', {'post':post})
