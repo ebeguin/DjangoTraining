353e1c550a700dedd0f7d8bd90215fcf7658c2f8
ebeguin <ebeguin@gmail.com>
2020-08-18 18:07:50 +0200

    setup static files

--- acme/prod_settings.py
+++ acme/prod_settings.py
@@ -6,6 +6,10 @@ TEMPLATE_DEBUG=False
 
 DATABASES['default'] = dj_database_url.config()
 
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ALLOWED_HOSTS = ["ace-tdn.herokuapp.com"]
 
 SECRET_KEY = 'PRODz(uak49u=_9m(4gq52+_yj#p-yh-2dv_%=li_c$ojek2)ksbh1'
