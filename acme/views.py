2d23c9206c877a13cb44361174f3792b7d17ab16
ebeguin <ebeguin@gmail.com>
2020-08-18 15:44:31 +0200

    fixing home route

--- acme/views.py
+++ acme/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 def home (request):
 	#return HttpResponse('HEllo acme')
-	return render(request,'pages\home.html')
+	return render(request,'pages/home.html')
 
 def handler404(request, exception):
 	return render(request, 'errors/404.html', {'error':exception}, status=404)
